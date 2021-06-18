b2e47b1fe7ebe151fa1a6f18e1bee86488bec4fd
Ali AlRasheed <ali.alrasheed@outlook.com>
2021-06-18 09:31:18 +0300

    Creating DesktopLauncher

---
+++ src/main/java/com/github/imbackt/pong/Boot.java
@@ -0,0 +1,10 @@
+package com.github.imbackt.pong;
+
+import com.badlogic.gdx.Game;
+
+public class Boot extends Game {
+    @Override
+    public void create() {
+
+    }
+}
---
+++ src/main/java/com/github/imbackt/pong/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.github.imbackt.pong;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setIdleFPS(60);
+        configuration.useVsync(true);
+        configuration.setTitle("Pong");
+
+        configuration.setWindowedMode(960, 640);
+        new Lwjgl3Application(new Boot(), configuration);
+    }
+}
