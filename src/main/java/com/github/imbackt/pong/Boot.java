8f2f0c287e15950b8841b7db6909ecd123d90916
Ali AlRasheed <ali.alrasheed@outlook.com>
2021-06-19 20:15:48 +0300

    Creating Game Screen

--- src/main/java/com/github/imbackt/pong/Boot.java
+++ src/main/java/com/github/imbackt/pong/Boot.java
@@ -1,10 +1,33 @@
 package com.github.imbackt.pong;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Boot extends Game {
-    @Override
-    public void create() {
 
+    public static Boot INSTANCE;
+    private int screenWidth;
+    private int screenHeight;
+    private OrthographicCamera camera;
+
+    public Boot() {
+        INSTANCE = this;
+    }
+
+    public void create() {
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, screenWidth, screenHeight);
+        setScreen(new GameScreen(camera));
+    }
+
+    public int getScreenWidth() {
+        return screenWidth;
+    }
+
+    public int getScreenHeight() {
+        return screenHeight;
     }
 }
